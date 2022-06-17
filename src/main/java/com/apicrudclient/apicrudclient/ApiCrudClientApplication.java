e4b81193d73e17e1c4add09eb6b508cc557d660e
Oliveira Bernaski <enoi@gft.com>
2022-06-17 16:07:13 -0300

    first commit

---
+++ src/main/java/com/apicrudclient/apicrudclient/ApiCrudClientApplication.java
@@ -0,0 +1,13 @@
+package com.apicrudclient.apicrudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apicrudclient/apicrudclient/ApiCrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicrudclient.apicrudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
